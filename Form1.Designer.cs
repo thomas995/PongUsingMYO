abdc35f5c39299170d8d98141316ea9ae4eac642
Thomas McNamara <G00314495@gmit.ie>
2017-02-21 20:40:09 +0000

    Finished Pong Game With Keyboard Controls Only

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,65 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.PlayerOne = new System.Windows.Forms.PictureBox();
-            this.PlayerTwo = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.Player1 = new System.Windows.Forms.PictureBox();
             this.Ball = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.PlayerOne)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.PlayerTwo)).BeginInit();
+            this.p2score = new System.Windows.Forms.Label();
+            this.p1Score = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
             this.SuspendLayout();
             // 
-            // PlayerOne
+            // Player2
             // 
-            this.PlayerOne.BackColor = System.Drawing.Color.Purple;
-            this.PlayerOne.Location = new System.Drawing.Point(133, 12);
-            this.PlayerOne.Name = "PlayerOne";
-            this.PlayerOne.Size = new System.Drawing.Size(100, 50);
-            this.PlayerOne.TabIndex = 0;
-            this.PlayerOne.TabStop = false;
+            this.Player2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Player2.BackColor = System.Drawing.Color.Crimson;
+            this.Player2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Player2.Location = new System.Drawing.Point(360, 32);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(120, 21);
+            this.Player2.TabIndex = 0;
+            this.Player2.TabStop = false;
             // 
-            // PlayerTwo
+            // Player1
             // 
-            this.PlayerTwo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.PlayerTwo.Location = new System.Drawing.Point(133, 228);
-            this.PlayerTwo.Name = "PlayerTwo";
-            this.PlayerTwo.Size = new System.Drawing.Size(100, 50);
-            this.PlayerTwo.TabIndex = 1;
-            this.PlayerTwo.TabStop = false;
+            this.Player1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Player1.BackColor = System.Drawing.Color.SpringGreen;
+            this.Player1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Player1.Location = new System.Drawing.Point(360, 308);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(120, 21);
+            this.Player1.TabIndex = 1;
+            this.Player1.TabStop = false;
             // 
             // Ball
             // 
-            this.Ball.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.Ball.Location = new System.Drawing.Point(133, 114);
+            this.Ball.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Ball.BackColor = System.Drawing.Color.Black;
+            this.Ball.Location = new System.Drawing.Point(408, 162);
             this.Ball.Name = "Ball";
-            this.Ball.Size = new System.Drawing.Size(100, 50);
+            this.Ball.Size = new System.Drawing.Size(30, 27);
             this.Ball.TabIndex = 2;
             this.Ball.TabStop = false;
+            // 
+            // p2score
+            // 
+            this.p2score.AutoSize = true;
+            this.p2score.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2score.Location = new System.Drawing.Point(-2, -2);
+            this.p2score.Name = "p2score";
+            this.p2score.Size = new System.Drawing.Size(27, 29);
+            this.p2score.TabIndex = 3;
+            this.p2score.Text = "0";
+            // 
+            // p1Score
+            // 
+            this.p1Score.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.p1Score.AutoSize = true;
+            this.p1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1Score.Location = new System.Drawing.Point(-2, 333);
+            this.p1Score.Name = "p1Score";
+            this.p1Score.Size = new System.Drawing.Size(27, 29);
+            this.p1Score.TabIndex = 4;
+            this.p1Score.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Player 2";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Player 1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 290);
+            this.ClientSize = new System.Drawing.Size(823, 358);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p1Score);
+            this.Controls.Add(this.p2score);
             this.Controls.Add(this.Ball);
-            this.Controls.Add(this.PlayerTwo);
-            this.Controls.Add(this.PlayerOne);
+            this.Controls.Add(this.Player1);
+            this.Controls.Add(this.Player2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.PlayerOne)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.PlayerTwo)).EndInit();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox PlayerOne;
-        private System.Windows.Forms.PictureBox PlayerTwo;
+        private System.Windows.Forms.PictureBox Player2;
+        private System.Windows.Forms.PictureBox Player1;
         private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Label p2score;
+        private System.Windows.Forms.Label p1Score;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,23 +17,88 @@ namespace PongWithMyo
 {
     public partial class Form1 : Form
     {
-
+        #region VARIABLES
         IChannel myoChannel;
         IHub myoHub;
+        
+        int PlayerSpeed = 8;
+        //int BallSpeed;
+
+        int P1Vel;
+        int P2Vel;
+
+        int bVelX = 2;
+        int bVelY = 2;
+        
+        int display = 0;
+        int ScoreOne;
+        int ScoreTwo;
+        
+        bool Pause;
+        #endregion
 
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();       
         }
 
-    
         private void Form1_Load(object sender, EventArgs e)
         {
             // InitializeMyo();
 
-            
+            Pause = true;
+            if (display == 0)
+            {
+                //MessageBox.Show("PRESS SPACE TO BEGIN GAME");
+                display++;
+            }
         }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (!Pause)
+           {
+                Ball.Location = new Point(Ball.Location.X + bVelX, Ball.Location.Y + bVelY);
+                
+                Player1.Location = new Point(Player1.Location.X + P1Vel, Player1.Location.Y);
+                
+                Player2.Location = new Point(Player2.Location.X + P2Vel, Player2.Location.Y);
+            }
+            if(Ball.Location.Y < 0)
+            {
+                ScoreOne++;
+                p1Score.Text = ScoreOne.ToString();
+                Ball.Width-=2;
+                Ball.Height-=2;
+                Ball.Location = new Point(this.Width / 2, this.Height / 2);
+            }
+            if (Ball.Location.Y > this.Height)
+            {
+                ScoreTwo++;
+                p2score.Text = ScoreTwo.ToString();
+                Ball.Width--;
+                Ball.Height--;
+                Ball.Location = new Point(this.Width / 2, this.Height / 2);
+            }
 
+            if (Ball.Location.X > Player1.Location.X && Ball.Location.X + Ball.Width < Player1.Location.X + Player1.Width && Ball.Location.Y + Ball.Height > Player1.Location.Y)
+            {
+                bVelY *= -1;
+            }
+            if (Ball.Location.X > Player2.Location.X && Ball.Location.X + Ball.Width < Player2.Location.X + Player2.Width && Ball.Location.Y < Player2.Location.Y + Player2.Height)
+            {
+                bVelY *= -1;
+            }
+            if(Ball.Location.X < 0)
+            {
+                bVelX *= -1;
+            }
+            if(Ball.Location.X + Ball.Width > this.Width)
+            {
+                bVelX *= -1;
+            }
+
+        }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             StopMyo();
@@ -48,8 +113,6 @@ namespace PongWithMyo
             myoHub.MyoDisconnected += myoHub_MyoDisconnected;
 
             myoChannel.StartListening();
-
-            
         }
 
         #region CONNECT/DISCONNECT MYO
@@ -70,6 +133,61 @@ namespace PongWithMyo
         {
             //myoChannel.StopListening();
             //myoChannel.Dispose();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            #region PLAYER MOVEMENT
+            if (e.KeyCode == Keys.Right)
+            {
+                P1Vel = PlayerSpeed;
+            }
+            else if((e.KeyCode == Keys.Left))
+            {
+                P1Vel = -PlayerSpeed;
+            }
+            else if((e.KeyCode == Keys.A))
+            {
+                P2Vel = -PlayerSpeed;
+            }
+            else if((e.KeyCode == Keys.D))
+            {
+                P2Vel = PlayerSpeed;
+            }
+            #endregion
+            #region PAUSING GAME
+            else if ((e.KeyCode == Keys.Space))
+            {
+                if (!Pause)
+                {
+                    Pause = true;
+                }
+                else if (Pause)
+                {
+                    Pause = false;
+                }
+            }
+            #endregion
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                P1Vel = 0;
+            }
+            else if ((e.KeyCode == Keys.Left))
+            {
+                P1Vel = 0;
+            }
+            else if ((e.KeyCode == Keys.A))
+            {
+                P2Vel = 0;
+            }
+            else if ((e.KeyCode == Keys.D))
+            {
+                P2Vel = 0;
+            }
         }
     }
 }
